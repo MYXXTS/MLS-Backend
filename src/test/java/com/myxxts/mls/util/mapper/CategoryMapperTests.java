a2b1a6bea085fdf5b77a4d54b800d85a5b0f61bf
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2024-02-20 23:52:35 +0800

    test: add category mapper converter test

---
+++ src/test/java/com/myxxts/mls/util/mapper/CategoryMapperTests.java
@@ -0,0 +1,68 @@
+package com.myxxts.mls.util.mapper;
+
+import com.myxxts.mls.module.core.category.CategoryEntity;
+import com.myxxts.mls.module.core.category.vo.CategoryConsoleListVo;
+import com.myxxts.mls.module.core.category.vo.CategoryViewListVo;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class CategoryMapperTests {
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Test
+    void testCategoryEntityToVoMapper() {
+        ObjectId userId = new ObjectId();
+        CategoryEntity categoryEntity = new CategoryEntity(
+                userId,
+                userId,
+                "test category",
+                "test category description"
+        );
+        CategoryEntity childCategoryEntity = new CategoryEntity(
+                userId,
+                userId,
+                "test child category",
+                "test child category description"
+        );
+        CategoryEntity categoryEntity1 = new CategoryEntity(
+                userId,
+                userId,
+                childCategoryEntity,
+                "test category 1",
+                "test category description 1"
+        );
+        System.out.println(categoryEntity);
+        System.out.println("======================================================");
+
+        System.out.println(childCategoryEntity);
+        System.out.println("======================================================");
+
+        System.out.println(categoryEntity1);
+        System.out.println("======================================================");
+
+        CategoryConsoleListVo categoryConsoleListVo = categoryMapper.toCategoryConsoleListVo(categoryEntity1);
+        System.out.println(categoryConsoleListVo.toString());
+        System.out.println("======================================================");
+
+        CategoryViewListVo categoryViewListVo = categoryMapper.toCategoryViewListVo(categoryEntity1);
+        System.out.println(categoryViewListVo.toString());
+        System.out.println("======================================================");
+
+        List<CategoryEntity> categoryEntities = List.of(categoryEntity, categoryEntity1);
+        System.out.println(categoryEntities);
+        System.out.println("======================================================");
+
+        List<CategoryConsoleListVo> categoryConsoleListVos = categoryMapper.toCategoryConsoleListVo(categoryEntities);
+        System.out.println(categoryConsoleListVos.toString());
+        System.out.println("======================================================");
+
+        List<CategoryViewListVo> categoryViewListVos = categoryMapper.toCategoryViewListVo(categoryEntities);
+        System.out.println(categoryViewListVos.toString());
+    }
+}
