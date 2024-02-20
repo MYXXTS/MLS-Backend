240f2dfcf8660be7582727d9f27cc84e03a9f33e
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2024-02-20 23:52:20 +0800

    feat(mapper): add category mapper converter

---
+++ src/main/java/com/myxxts/mls/util/mapper/CategoryMapper.java
@@ -0,0 +1,65 @@
+package com.myxxts.mls.util.mapper;
+
+import com.myxxts.mls.module.core.category.CategoryEntity;
+import com.myxxts.mls.module.core.category.vo.CategoryConsoleListVo;
+import com.myxxts.mls.module.core.category.vo.CategoryViewListVo;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public abstract class CategoryMapper {
+
+    public CategoryConsoleListVo toCategoryConsoleListVo(CategoryEntity categoryEntity){
+        if (categoryEntity == null) {
+            return null;
+        }
+
+        CategoryConsoleListVo categoryConsoleListVo = new CategoryConsoleListVo();
+
+        categoryConsoleListVo.setCreateBy( categoryEntity.getCreateBy() );
+        categoryConsoleListVo.setUpdateBy( categoryEntity.getUpdateBy() );
+        categoryConsoleListVo.setCreateTime( categoryEntity.getCreateTime() );
+        categoryConsoleListVo.setUpdateTime( categoryEntity.getUpdateTime() );
+        categoryConsoleListVo.setCid( categoryEntity.getCid() );
+        categoryConsoleListVo.setArticleCount( categoryEntity.getArticleCount() );
+        categoryConsoleListVo.setName( categoryEntity.getName() );
+        categoryConsoleListVo.setDescription( categoryEntity.getDescription() );
+
+        CategoryEntity child = categoryEntity.getChild();
+        if ( child != null ) {
+            categoryConsoleListVo.setChild( toCategoryConsoleListVo( child ) );
+        }
+
+        return categoryConsoleListVo;
+    }
+
+    public abstract List<CategoryConsoleListVo> toCategoryConsoleListVo(List<CategoryEntity> categoryEntity);
+
+    public CategoryViewListVo toCategoryViewListVo(CategoryEntity categoryEntity){
+        if ( categoryEntity == null ) {
+            return null;
+        }
+
+        CategoryViewListVo categoryViewListVo = new CategoryViewListVo();
+
+        categoryViewListVo.setCreateBy( categoryEntity.getCreateBy() );
+        categoryViewListVo.setUpdateBy( categoryEntity.getUpdateBy() );
+        categoryViewListVo.setCreateTime( categoryEntity.getCreateTime() );
+        categoryViewListVo.setUpdateTime( categoryEntity.getUpdateTime() );
+        categoryViewListVo.setCid( categoryEntity.getCid() );
+        categoryViewListVo.setName( categoryEntity.getName() );
+        categoryViewListVo.setDescription( categoryEntity.getDescription() );
+        categoryViewListVo.setRoute( categoryEntity.getRoute() );
+
+        CategoryEntity child = categoryEntity.getChild();
+
+        if ( child != null ) {
+            categoryViewListVo.setChild( toCategoryViewListVo( child ) );
+        }
+
+        return categoryViewListVo;
+    }
+
+    public abstract List<CategoryViewListVo> toCategoryViewListVo(List<CategoryEntity> categoryEntity);
+}
