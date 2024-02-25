bac5d9e56f8351435ddf9df618d6cedfcbdf90dd
MYXXTS <51087760+MYXXTS@users.noreply.github.com>
2024-02-25 22:53:24 +0800

    feat(repository): add query function about article

--- src/main/java/com/myxxts/mls/repository/core/ArticleRepository.java
+++ src/main/java/com/myxxts/mls/repository/core/ArticleRepository.java
@@ -1,9 +1,24 @@
 package com.myxxts.mls.repository.core;
 
 import com.myxxts.mls.module.core.article.ArticleEntity;
+import jakarta.validation.constraints.NotNull;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.validation.annotation.Validated;
 
+import java.util.List;
+
+@Validated
 public interface ArticleRepository extends MongoRepository<ArticleEntity, ObjectId> {
+    @Query(value = "{'tags.name': { $in: ?0 }, 'categories.name': { $in: ?1 }}")
+    List<ArticleEntity> findAllByTagsAndCategories(@NotNull List<String> tags, @NotNull List<String> categories, PageRequest pageRequest);
+
+    @Query(value = "{'tags.name': { $in: ?0 }}")
+    List<ArticleEntity> findAllByTags(@NotNull List<String> tags, PageRequest pageRequest);
+
+    @Query(value = "{ 'categories.name': { $in: ?0 }}")
+    List<ArticleEntity> findAllByCategories(@NotNull List<String> categories, PageRequest pageRequest);
 
 }
